216162ac3f2733cc5c491f4d2fa060c9214331af
adam <kowal2991@gmail.com>
2025-11-20 12:52:38 +0100

    screen system

--- AmazingMazeDesktop/Main.cs
+++ AmazingMazeDesktop/Main.cs
@@ -15,17 +15,12 @@ namespace AmazingMazeDesktop;
 public class Main : Game
 {
     private GraphicsDeviceManager _graphics;
-    public SpriteBatch SpriteBatch;
     private ScreenManager _screenManager;
     private AnimationController _spellcastAnimationController;
 
 
-    private OrthographicCamera _camera;
-
     //private Camera2D _camera;
-    private GameContext _gameContext;
-    private ConfigsPackage _configs;
-    private World _worldLast;
+
 
     public Main()
     {
@@ -48,22 +43,6 @@ public class Main : Game
 
         GlobalRng.Initialize(12345);
 
-        _configs = new ConfigsPackage()
-        {
-            DungeonConfig = new DungeonConfig()
-            {
-                Seed = 1234
-            },
-            LevelConfig = new LevelConfig()
-            {
-                Seed = 1234,
-                Height = 30,
-                Width = 30,
-            }
-        };
-
-        _camera = new OrthographicCamera(GraphicsDevice);
-
 
         base.Initialize();
     }
@@ -71,36 +50,6 @@ public class Main : Game
 
     protected override void LoadContent()
     {
-        SpriteBatch = new SpriteBatch(GraphicsDevice);
-        Assets.Load(Content);
-        Assets.OrangePlaceholderTexture = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
-        Assets.OrangePlaceholderTexture.SetData([Color.MonoGameOrange]);
-        Assets.WhitePlaceholderTexture = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
-        Assets.WhitePlaceholderTexture.SetData([Color.White]);
-        Assets.YellowPlaceholderTexture = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
-        Assets.YellowPlaceholderTexture.SetData([Color.Yellow]);
-        Assets.GreenPlaceholderTexture = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
-        Assets.GreenPlaceholderTexture.SetData([Color.Green]);
-
-        _gameContext = new GameContext(_configs, _camera, GraphicsDevice);
-
-        _worldLast = _gameContext.World.World;
-        Components.Add(_worldLast);
-
-        // Debug menu
-        // debugSettingsPanel = new Panel();
-        //
-        // debugSettingsPanel.Dock(Dock.Fill);
-        // debugSettingsPanel.AddToRoot();
-        // StackPanel stackPanel = new StackPanel();
-        // debugSettingsPanel.AddChild(stackPanel);
-        // stackPanel.Orientation = Orientation.Vertical;
-        // var button = new Button();
-        // button.Anchor(Anchor.Top);
-        // stackPanel.AddChild(button);
-        // var button2 = new Button();
-        // button2.Anchor(Anchor.Top);
-        // stackPanel.AddChild(button2);
     }
 
     protected override void Update(GameTime gameTime)
@@ -111,76 +60,12 @@ public class Main : Game
 
         KeyboardExtended.Update();
         MouseExtended.Update();
-        // _gameContext.Update(gameTime);
-        // if (_worldLast != _gameContext.World.World)
-        // {
-        //     //Components.Remove(_gameContext.World.World);
-        //     Components.Remove(_worldLast);
-        //     _worldLast = _gameContext.World.World;
-        //     Components.Add(_gameContext.World.World);
-        // }
-
-
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        // _gameContext.Draw(gameTime);
-        //
-        // SpriteBatch.Begin(transformMatrix: _camera.GetViewMatrix());
-        // // Draw maze background
-        // // for (var y = 0; y < _maze.MazeSchema.GetLength(0); y++)
-        // // {
-        // //     for (var x = 0; x < _maze.MazeSchema.GetLength(1); x++)
-        // //     {
-        // //         Debug.Write(_maze.MazeSchema[y, x]);
-        // //     }
-        // //     Debug.Write("\n");
-        // // }
-        // for (var y = 0; y < _gameContext.CurrentLevel.MazeStructure.Map.GetLength(0); y++)
-        // {
-        //     for (var x = 0; x < _gameContext.CurrentLevel.MazeStructure.Map.GetLength(1); x++)
-        //     {
-        //         switch (_gameContext.CurrentLevel.MazeStructure.Map[y, x])
-        //         {
-        //             case 0:
-        //                 SpriteBatch.Draw(Assets.WhitePlaceholderTexture, new Vector2(x, y) * EngineSettings.CellSize,
-        //                     null, Color.White, 0f,
-        //                     Vector2.Zero, EngineSettings.CellSize, SpriteEffects.None, 1f);
-        //                 break;
-        //             case 1:
-        //                 SpriteBatch.Draw(Assets.OrangePlaceholderTexture, new Vector2(x, y) * EngineSettings.CellSize,
-        //                     null, Color.White, 0f,
-        //                     Vector2.Zero, EngineSettings.CellSize, SpriteEffects.None, 1f);
-        //                 break;
-        //             case >= 2:
-        //                 if (_gameContext.CurrentLevel.EntryRoomId ==
-        //                     _gameContext.CurrentLevel.MazeStructure.Map[y, x] ||
-        //                     _gameContext.CurrentLevel.ExitRoomId == _gameContext.CurrentLevel.MazeStructure.Map[y, x])
-        //                 {
-        //                     SpriteBatch.Draw(Assets.GreenPlaceholderTexture,
-        //                         new Vector2(x, y) * EngineSettings.CellSize,
-        //                         null, Color.White, 0f,
-        //                         Vector2.Zero, EngineSettings.CellSize, SpriteEffects.None, 1f);
-        //                 }
-        //                 else
-        //                 {
-        //                     SpriteBatch.Draw(Assets.YellowPlaceholderTexture,
-        //                         new Vector2(x, y) * EngineSettings.CellSize,
-        //                         null, Color.White, 0f,
-        //                         Vector2.Zero, EngineSettings.CellSize, SpriteEffects.None, 1f);
-        //                 }
-        //
-        //                 break;
-        //         }
-        //     }
-        // }
-        //
-        // SpriteBatch.End();
-
         base.Draw(gameTime);
     }
 }
--- AmazingMazeDesktop/Screens/GameplayScreen.cs
+++ AmazingMazeDesktop/Screens/GameplayScreen.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Linq;
 using AmazingMazeDesktop.UI;
+using AmazingMazeDesktop.WorldGeneration.Configs;
 using Gum.Forms;
 using Gum.Forms.Controls;
 using Gum.Managers;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.ECS;
 using MonoGame.Extended.Input;
 using MonoGame.Extended.Screens;
 using MonoGameGum;
@@ -17,13 +20,50 @@ public class GameplayScreen(Game game) : GameScreen(game)
 {
     private SpriteBatch _spriteBatch;
     private DebugUi _debugUi = new DebugUi(game);
+    private GameContext _gameContext;
+    private OrthographicCamera _camera;
+    private ConfigsPackage _configs;
+    private World _worldLast;
 
     public override void Initialize()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        _debugUi.Initialize();
-        
+        _camera = new OrthographicCamera(GraphicsDevice);
         base.Initialize();
+    }
+
+    public override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _configs = new ConfigsPackage()
+        {
+            DungeonConfig = new DungeonConfig()
+            {
+                Seed = 1234
+            },
+            LevelConfig = new LevelConfig()
+            {
+                Seed = 1234,
+                Height = 30,
+                Width = 30,
+            }
+        };
+
+        Assets.Load(Content);
+        Assets.OrangePlaceholderTexture = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+        Assets.OrangePlaceholderTexture.SetData([Color.MonoGameOrange]);
+        Assets.WhitePlaceholderTexture = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+        Assets.WhitePlaceholderTexture.SetData([Color.White]);
+        Assets.YellowPlaceholderTexture = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+        Assets.YellowPlaceholderTexture.SetData([Color.Yellow]);
+        Assets.GreenPlaceholderTexture = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+        Assets.GreenPlaceholderTexture.SetData([Color.Green]);
+
+        _gameContext = new GameContext(_configs, _camera, GraphicsDevice);
+        _worldLast = _gameContext.World.World;
+        //game.Components.Add(_worldLast);
+        _debugUi.Initialize();
+        base.LoadContent();
     }
 
     public override void Update(GameTime gameTime)
@@ -33,12 +73,66 @@ public class GameplayScreen(Game game) : GameScreen(game)
             _debugUi.SwitchVisibility();
         }
 
+        _gameContext.Update(gameTime);
+        if (_worldLast != _gameContext.World.World)
+        {
+            
+            //Components.Remove(_gameContext.World.World);
+            //game.Components.Remove(_worldLast);
+            _worldLast = _gameContext.World.World;
+            //game.Components.Add(_gameContext.World.World);
+        }
+
+        _worldLast.Update(gameTime);
         _debugUi.Update(gameTime);
     }
 
     public override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.Green);
+        GraphicsDevice.Clear(Color.BlanchedAlmond);
+        _gameContext.Draw(gameTime);
+        _spriteBatch.Begin(transformMatrix: _camera.GetViewMatrix());
+        for (var y = 0; y < _gameContext.CurrentLevel.MazeStructure.Map.GetLength(0); y++)
+        {
+            for (var x = 0; x < _gameContext.CurrentLevel.MazeStructure.Map.GetLength(1); x++)
+            {
+                switch (_gameContext.CurrentLevel.MazeStructure.Map[y, x])
+                {
+                    case 0:
+                        _spriteBatch.Draw(Assets.WhitePlaceholderTexture, new Vector2(x, y) * EngineSettings.CellSize,
+                            null, Color.White, 0f,
+                            Vector2.Zero, EngineSettings.CellSize, SpriteEffects.None, 1f);
+                        break;
+                    case 1:
+                        _spriteBatch.Draw(Assets.OrangePlaceholderTexture, new Vector2(x, y) * EngineSettings.CellSize,
+                            null, Color.White, 0f,
+                            Vector2.Zero, EngineSettings.CellSize, SpriteEffects.None, 1f);
+                        break;
+                    case >= 2:
+                        if (_gameContext.CurrentLevel.EntryRoomId ==
+                            _gameContext.CurrentLevel.MazeStructure.Map[y, x] ||
+                            _gameContext.CurrentLevel.ExitRoomId == _gameContext.CurrentLevel.MazeStructure.Map[y, x])
+                        {
+                            _spriteBatch.Draw(Assets.GreenPlaceholderTexture,
+                                new Vector2(x, y) * EngineSettings.CellSize,
+                                null, Color.White, 0f,
+                                Vector2.Zero, EngineSettings.CellSize, SpriteEffects.None, 1f);
+                        }
+                        else
+                        {
+                            _spriteBatch.Draw(Assets.YellowPlaceholderTexture,
+                                new Vector2(x, y) * EngineSettings.CellSize,
+                                null, Color.White, 0f,
+                                Vector2.Zero, EngineSettings.CellSize, SpriteEffects.None, 1f);
+                        }
+
+                        break;
+                }
+            }
+        }
+
+        _spriteBatch.End();
+        _worldLast.Draw(gameTime);
         _debugUi.Draw();
     }
 }
